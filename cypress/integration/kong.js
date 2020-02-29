3384490c668223e75841e1fcaaef1d81c3da9302
rml1997 <r@blevy.co.uk>
2020-02-29 18:13:32 +0000

    trying to get the link

--- cypress/integration/kong.js
+++ cypress/integration/kong.js
@@ -12,7 +12,7 @@ describe('Kongregate BOTD getter', function() {
     cy.get('#welcome_box_sign_in_button')
       .click({force: true})
     
-    let link = cy.get('.badge_link .click_link').href
+    let link = cy.get('.botd_title > a').href
     
     cy.visit(link+'/chat_achievements')
   })
