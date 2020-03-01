5c0d625dea2b29ed5533ce7ec6794ad35b6ba318
rml1997 <r@blevy.co.uk>
2020-03-01 07:48:49 +0000

    check for avatar for login

--- cypress/integration/kong.js
+++ cypress/integration/kong.js
@@ -12,7 +12,7 @@ describe('Kongregate BOTD getter', function() {
     cy.get('#welcome_box_sign_in_button')
       .click({force: true})
     
-    cy.get('.username_holder').contains(Cypress.env('kong_user'))
+    cy.get('.welcome_box_small_user_avatar')
     
     cy.get('.botd_title > strong > a').invoke('attr', 'href')
       .then(link => cy.visit( link + '/chat_achievements'))
