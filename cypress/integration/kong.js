f6bd8fb915c2dcb9cf836d2851596e6a91e17760
rml1997 <r@blevy.co.uk>
2020-03-01 07:42:05 +0000

    wait for login ajax

--- cypress/integration/kong.js
+++ cypress/integration/kong.js
@@ -12,6 +12,8 @@ describe('Kongregate BOTD getter', function() {
     cy.get('#welcome_box_sign_in_button')
       .click({force: true})
     
+    cy.get('.username_holder').contains(Cypress.env('kong_user'))
+    
     cy.get('.botd_title > strong > a').invoke('attr', 'href')
       .then(link => cy.visit( link + '/chat_achievements'))
   })
