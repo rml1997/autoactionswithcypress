94acd7a2d9ac0c92b623f33527c3beca98bc97aa
rml1997 <r@blevy.co.uk>
2020-03-01 08:48:08 +0000

    epic games added a captcha

--- cypress/integration/epic.js
+++
@@ -1,5 +0,0 @@
-describe('Epic Games free game getter', function() {
-  it('Logs in, gets free games you need', function() {
-    cy.visit('https://www.epicgames.com/store/en-US/free-games')    
-  })
-})
