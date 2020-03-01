50fd916a8d7bc86e24cc834e4dfa1c3ca16107ac
rml1997 <r@blevy.co.uk>
2020-03-01 07:55:18 +0000

    use later login page

--- cypress/integration/kong.js
+++ cypress/integration/kong.js
@@ -3,18 +3,16 @@ describe('Kongregate BOTD getter', function() {
     
     cy.visit('https://kongregate.com/badges')
     
-    cy.get('#welcome_username')
-      .type(Cypress.env('kong_user'),{force: true})
-    
-    cy.get('#welcome_password')
-      .type(Cypress.env('kong_pass'),{force: true})
-    
-    cy.get('#welcome_box_sign_in_button')
-      .click({force: true})
-    
-    cy.get('.welcome_box_small_user_avatar')
-    
     cy.get('.botd_title > strong > a').invoke('attr', 'href')
       .then(link => cy.visit( link + '/chat_achievements'))
+
+    cy.get('#username')
+      .type(Cypress.env('kong_user'),{force: true})
+    
+    cy.get('#password')
+      .type(Cypress.env('kong_pass'),{force: true})
+    
+    cy.get('#sessions_new_form_spinner')
+      .click({force: true})
   })
 })
